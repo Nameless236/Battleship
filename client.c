5e5505302e2011bd416dd0cc7f4bea2296db873b
JakubPastorek <pastojakub@gmail.com>
2025-01-03 13:32:14 +0100

    Connect to server

--- client.c
+++ client.c
@@ -1,1 +1,16 @@
 #include "klient.h"
+#include "pipe.h"
+
+int connect_to_server(const char *server_ip, int port) {
+    char pipe_path[256];
+    snprintf(pipe_path, sizeof(pipe_path), "/tmp/server_%d", port);
+
+    pipe_init(pipe_path);
+    int fd = pipe_open_write(pipe_path);
+    if (fd == -1) {
+        pipe_destroy(pipe_path);
+        return -1;
+    }
+
+    return fd;
+}
